d4167927539dd2cd7dc30eadd2eb6f97ed1014f5
Patryk Frejek <freju13@gmail.com>
2023-04-14 12:47:53 +0200

    video add

--- starter/script.js
+++ starter/script.js
@@ -2,7 +2,13 @@
 window.onload = init;
 const toggle = document.querySelector(".btn-menu");
 const navbar = document.querySelector(".navigation__list");
+window.onresize = ()=>{
+storyVideoResize();
+}
+
+
 function init() {
+storyVideoResize();
 
 	toggle.onclick = () => {
 		navbar.classList.toggle("active");
@@ -11,3 +17,10 @@ function init() {
 }
 
 
+function storyVideoResize(){
+	let video = document.getElementById("story-video");
+		if (window.innerWidth <= 600) {
+			let width = window.innerWidth - 30 + "";
+			video.setAttribute("width", `${width}`);
+		}
+}
